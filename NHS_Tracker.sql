d5ece1d8543e8a6da60788392fcf51d40cb4a6c4
Lucy Werner <l.a.werner@wlv.ac.uk>
2020-03-27 16:01:59 +0000

    The start of the FK's
    have started writing the FK constraints

--- NHS_Tracker.sql
+++ NHS_Tracker.sql
@@ -1,15 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 4.6.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Mar 13, 2020 at 12:13 PM
--- Server version: 5.7.24-0ubuntu0.16.04.1
--- PHP Version: 7.0.32-0ubuntu0.16.04.1
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,30 +7,25 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db1824591`
+-- Database: `db_nhs_asset_tracker`
 --
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `Department`
 --
-
 CREATE TABLE `Department` (
-  `PK_Dept_ID` int(3) NOT NULL,
+  `Dept_ID` int(3) NOT NULL,
   `Name` varchar(15) NOT NULL,
-  `FLOOR` varchar(2) NOT NULL,
-  `Ward` varchar(3) NOT NULL
+  `Floor` varchar(2) NOT NULL,
+  `Ward` varchar(3) NOT NULL,
+  `Hospital_ID` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `Equipment`
 --
-
 CREATE TABLE `Equipment` (
-  `PK_ID` varchar(15) NOT NULL,
+  `Equip_ID` varchar(15) NOT NULL,
   `Barcode` varchar(12) NOT NULL,
   `Name` varchar(20) NOT NULL,
   `Category` varchar(25) NOT NULL,
@@ -51,13 +36,10 @@ CREATE TABLE `Equipment` (
   `Return_due` datetime DEFAULT NULL,
   `Last_Cleaned` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `Hospital`
 --
-
 CREATE TABLE `Hospital` (
   `PK_Hosp_ID` int(10) NOT NULL,
   `UN_Hospital_ID` varchar(10) NOT NULL,
@@ -66,36 +48,27 @@ CREATE TABLE `Hospital` (
   `Town` varchar(15) DEFAULT NULL,
   `County` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `IDs`
 --
-
 CREATE TABLE `IDs` (
   `Patient_ID` varchar(10) NOT NULL,
   `Staff_ID` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `Maintenance`
 --
-
 CREATE TABLE `Maintenance` (
   `Issue #` varchar(2) NOT NULL,
   `Details` varchar(60) NOT NULL,
   `Cleaning_Record` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `Person`
 --
-
 CREATE TABLE `Person` (
   `PK_Pats_ID` int(10) NOT NULL,
   `Surname` varchar(15) NOT NULL,
@@ -104,20 +77,14 @@ CREATE TABLE `Person` (
   `Town` varchar(15) DEFAULT NULL,
   `County` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `User`
 --
-
 CREATE TABLE `User` (
   `Username` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
 --
 -- Indexes for table `Department`
@@ -150,6 +117,18 @@ ALTER TABLE `IDs`
 ALTER TABLE `Person`
   ADD PRIMARY KEY (`PK_Pats_ID`);
 
+-- -----------------------------------------------------------------------
+--
+--      Foreign Key Constraints 
+--
+-- Department
+ALTER TABLE `Department`
+  ADD CONSTRAINT `dept_hosp` FOREIGN KEY (`Hospital_ID`)
+  REFERENCES `Hospital` (`Hospital_ID) ON UPDATE CASCADE;
+
+--
+--
+--
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
